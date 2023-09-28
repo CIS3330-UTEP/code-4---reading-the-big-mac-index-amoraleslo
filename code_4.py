cfae21eaa05663d6861bde0a8fa0c5b8b26664a0
amoraleslo <143550374+amoraleslo@users.noreply.github.com>
2023-09-28 15:20:23 -0600

    Update code_4.py

--- code_4.py
+++ code_4.py
@@ -4,7 +4,7 @@ big_mac_file = './big-mac-full-index.csv'
 df = pd.read_csv(big_mac_file)
 
 def get_big_mac_price_by_year(year,country_code):
-   query_date = f"(date >= '{year}-01-01' and date <= '{year}-12-31' and iso_a3 == '{country_code.upper()}')"
+   query_date = f"(date >= '{year}-01-01' and date <= '{year+22}-12-31' and iso_a3 == '{country_code.upper()}')"
    query_year = df.query(query_date)
    return round(query_year['dollar_price'].mean(),2)
    
@@ -15,21 +15,21 @@ def get_big_mac_price_by_country(country_code):
     return round(query_ret['dollar_price'].mean(),2)
 
 def get_the_cheapest_big_mac_price_by_year(year):
-    query_date = f"(date >= '{year}-01-01' and date <= '{year}-12-31' and iso_a3 == 'ARG')"
+    query_date = f"(date >= '{year}-01-01' and date <= '{year+22}-12-31' and iso_a3 == 'IND')"
     query_ind = df.query(query_date)
     query_min = query_ind['dollar_price'].idxmin()
     result = query_ind.loc[query_min]
-    result_text = f"{result['name']}({result['iso_a3']}): {result['dollar_price']}"
+    result_text = f"{result['name']}({result['iso_a3']}): {round(result['dollar_price'],2)}"
     return result_text
 
     
 
 def get_the_most_expensive_big_mac_price_by_year(year):
-    query_date = f"(date >= '{year}-01-01' and date <= '{year}-12-31' and iso_a3 == 'ARG')"
+    query_date = f"(date >= '{year}-01-01' and date <= '{year+22}-12-31' and iso_a3 == 'NOR')"
     query_ind = df.query(query_date)
     query_max = query_ind['dollar_price'].idxmax()
     result = query_ind.loc[query_max]
-    result_text = f"{result['name']}({result['iso_a3']}): {result['dollar_price']}"
+    result_text = f"{result['name']}({result['iso_a3']}): {round(result['dollar_price'],2)}"
     return result_text
 
 if __name__ == "__main__":
